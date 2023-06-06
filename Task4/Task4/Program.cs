9637d689db3ba416311e95bc11de1af0f1032f14
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-06-07 00:41:23 +0300

    Task4

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Hp\\Documents\\GitHub\\3Modul\\HomeTask\\HomeTask1\\Directory\\HomeWork1";
+            int count = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.ReadLine() != null)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($" Qatorlar soni: {count}");
+        }
+    }
+}
