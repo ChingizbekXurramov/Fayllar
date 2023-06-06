89b257b017228a7d76cbf34d4f44f678157a1dca
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-06-07 00:32:55 +0300

    Task2

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Hp\\Documents\\GitHub\\";
+            Determination(path);
+        }
+        static void Determination(string path)
+        {
+            string[] folder = Directory.GetDirectories(path);
+            string[] file = Directory.GetFiles(path);
+
+            if (folder.Length != 0 && file.Length != 0)
+                Console.WriteLine(" There are folders and filies.");
+
+            else if (folder.Length != 0)
+                Console.WriteLine(" There is a folder.");
+
+            else if (file.Length != 0)
+                Console.WriteLine(" There is a file..");
+
+            else
+                Console.WriteLine(" Folder and file are empty.");
+        }
+    }
+}
