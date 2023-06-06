6d01aab64b47fcf073af4e181f3dc129d5f5b142
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-06-07 00:38:47 +0300

    TAsk3

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string files1 = "C:\\Users\\Hp\\Documents\\GitHub\\3Modul\\HomeTask\\HomeTask1\\Directory\\HomeWork1\\Directory-DirectoryInfo1";
+            string files2 = "C:\\Users\\Hp\\Documents\\GitHub\\3Modul\\New";
+            File.Copy(files1, files2, true);
+        }
+    }
+}
