327df90249c6721cbc9e4958e61202299108f408
Chingizbek <128830130+ChingizbekXurramov@users.noreply.github.com>
2023-06-07 00:29:45 +0300

    Task1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "C:\\Users\\Hp\\Documents\\GitHub\\3Modul\\HomeTask\\.gitignore";
+            if (!Directory.Exists(file))
+            {
+                Directory.CreateDirectory(file);
+            }
+            else
+            {
+                Console.WriteLine("  File Exists ");
+            }
+        }
+    }
+}
